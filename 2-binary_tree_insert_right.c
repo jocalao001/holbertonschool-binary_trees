fb0eff026ad5a6f80d93f39a239e898d85f6b00a
jocalao001 <joca-001@hotmail.com>
2023-05-18 20:11:21 -0700

    fixedbetty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,8 @@
  * Return:                    Pointer to he created node, or NULL on failure or
  *                            if parent is NULL
  */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
 	binary_tree_t *newNode = NULL;
 
 	if (parent == NULL)
